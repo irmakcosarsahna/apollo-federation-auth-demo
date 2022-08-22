141364c6866571854b9a484d53f30d9291fded0e
Irmak Coşar Şahna <irmak@sahna.io>
2022-08-22 22:07:32 +0300

    qf

--- src/gateway/index.js
+++ src/gateway/index.js
@@ -4,7 +4,7 @@ const {ApolloGateway, IntrospectAndCompose} = require("@apollo/gateway");
 const gateway = new ApolloGateway({
     supergraphSdl: new IntrospectAndCompose({
         subgraphs: [
-            {name: "users", url: "http://localhost:9595/graphql"},
+            {name: "users", url: "http://localhost:9595/graphql"}
         ],
     }),
     __exposeQueryPlanExperimental: false,
@@ -12,13 +12,7 @@ const gateway = new ApolloGateway({
 
 const server = new ApolloServer({
     gateway,
-
-    // Apollo Graph Manager (previously known as Apollo Engine)
-    // When enabled and an `ENGINE_API_KEY` is set in the environment,
-    // provides metrics, schema management and trace reporting.
     engine: false,
-
-    // Subscriptions are unsupported but planned for a future Gateway version.
     subscriptions: false,
 });
 
